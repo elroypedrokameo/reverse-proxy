c51b9e6ee595b916ce9a5373ce6a6e8f29daad82
infinitygamexray <infinitygamexray@gmail.com>
2026-05-16 09:28:48 +0700

    refactor: replace Link component with anchor tag in Home component

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,8 @@
-import Image from "next/image";
-import Link from "next/link";
-
 export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       Page 1 di project utama
-      <Link href="/pagekedua">Akses ke Page 2 lewat reverse proxy</Link>
+      <a href="/pagekedua">Akses ke Page 2 lewat reverse proxy</a>
     </div>
   );
 }
