98a6cc9e63eb18d03a6ff450a61e8134485d9c7f
infinitygamexray <infinitygamexray@gmail.com>
2026-05-16 09:48:36 +0700

    refactor: enhance text content in Home component with additional details

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,11 @@ export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <p>
-        Page 1 di project utama yang saya deploy di vercel menggunakan next js
+        Page 1 di project utama yang saya deploy di <span className="text-red-500">vercel</span> menggunakan next js
       </p>
       <a href="/pagekedua" className="text-blue-500">
-        Akses ke Page 2 lewat reverse proxy
+        Akses ke Page 2 lewat reverse proxy, ini akan akses project 2 di{" "}
+        <span className="text-red-500">cloudflare</span>
       </a>
     </div>
   );
