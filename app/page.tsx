3a9fb8b2edb175185824e52b42bf9a9ee471f17a
infinitygamexray <infinitygamexray@gmail.com>
2026-05-16 09:29:25 +0700

    style: add text color to link in Home component

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       Page 1 di project utama
-      <a href="/pagekedua">Akses ke Page 2 lewat reverse proxy</a>
+      <a href="/pagekedua" className="text-blue-500">Akses ke Page 2 lewat reverse proxy</a>
     </div>
   );
 }
