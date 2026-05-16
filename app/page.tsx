c5e6f8691239ce31802e65ad10721e2b923b095e
infinitygamexray <infinitygamexray@gmail.com>
2026-05-16 09:46:09 +0700

    refactor: enhance content in Home component with additional description

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,12 @@
 export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      Page 1 di project utama
-      <a href="/pagekedua" className="text-blue-500">Akses ke Page 2 lewat reverse proxy</a>
+      <p>
+        Page 1 di project utama yang saya deploy di vercel menggunakan next js
+      </p>
+      <a href="/pagekedua" className="text-blue-500">
+        Akses ke Page 2 lewat reverse proxy
+      </a>
     </div>
   );
 }
