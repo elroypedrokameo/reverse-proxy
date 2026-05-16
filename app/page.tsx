bf05e590f20475e2476f978cee10ed5ef9ac62d4
infinitygamexray <infinitygamexray@gmail.com>
2026-05-16 10:31:15 +0700

    refactor: simplify text content in Home component for clarity

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,10 @@ export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <p>
-        Page 1 di project utama yang saya deploy di <span className="text-red-500">vercel</span> menggunakan next js
+        Project utama deploy di <span className="text-red-500">vercel</span>
       </p>
       <a href="/pagekedua" className="text-blue-500">
-        Akses ke Page 2 lewat reverse proxy, ini akan akses project 2 di{" "}
+        Akses project kedua yang deploy di
         <span className="text-red-500">cloudflare</span>
       </a>
     </div>
